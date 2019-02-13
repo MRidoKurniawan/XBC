74b3c42935aa8d52de33f89acb948fb5f0613b84
M Rido Kurniawan <mrkawan77@gmail.com>
2019-02-13 15:31:46 +0700

    XBC-WEB-183

---
+++ XBC/XBC.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XBC.MVC.Startup))]
+namespace XBC.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
