ceb34de5776a87f3fc21e4ec0e0de04f746a1ba1
M Rido Kurniawan <mrkawan77@gmail.com>
2019-02-14 09:55:55 +0700

    Tambahan  ResponseResult.cs

---
+++ XBC/XBC.ViewModel/ResponseResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XBC.ViewModel
+{
+    public class ResponseResult
+    {
+        public ResponseResult()
+        {
+            Success = true;
+        }
+
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+        public object Entity { get; set; }
+    }
+}
