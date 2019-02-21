5aa0566750c55558e9c573f5d039f8a3dfbea2dd
dearmhmdr <muhammadddear@gmail.com>
2019-02-21 09:09:03 +0700

    test

---
+++ XBC/XBC.MVC/Controllers/FeedbackController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XBC.Repository;
+
+namespace XBC.MVC.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return PartialView("_List", FeedbackRepo.All());
+        }
+
+        public ActionResult ChooseDocument(long id)
+        {
+            return PartialView("_ChooseDocument", FeedbackRepo.ListDT(id));
+        }
+
+        public ActionResult Add(long id)
+        {
+            return PartialView("_Add", FeedbackRepo.ListDTD(id));
+        }
+
+    }
+}
